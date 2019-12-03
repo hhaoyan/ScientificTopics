1bef2f96fe9af400a60123654506d005bd7c9fc9
Haoyan Huo <hhaoyann@gmail.com>
2019-12-03 15:31:03 -0800

    Modify model definition.

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+__author__ = 'Haoyan Huo'
+__maintainer__ = 'Haoyan Huo'
+__email__ = 'haoyan.huo@lbl.gov'
+
+if __name__ == '__main__':
+    setup(
+        name='ScientificTopics',
+        version="0.0.1",
+        python_requires='>=3.4',
+        author=__author__,
+        author_email=__email__,
+        packages=find_packages(),
+        zip_safe=False,
+        install_requires=[
+            'Cython',
+            'nltk',
+            'numpy',
+            'requests',
+            'sentencepiece',
+            'six',
+            'tqdm',
+            'pyyaml',
+        ]
+    )
